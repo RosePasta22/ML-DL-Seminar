d945b7ae4cfc13911578f705105376dee92cf3f6
RosePasta22 <danieljoo04@gmail.com>
2025-09-20 07:49:11 +0900

    Export API in __init__; update pyproject

--- Data_Research_Code/RobustLoss/robustloss/__init__.py
+++ Data_Research_Code/RobustLoss/robustloss/__init__.py
@@ -1,28 +1,41 @@
 # robustloss/__init__.py
-# Bind names explicitly into the package namespace (fixes import issues)
+# Bind names explicitly into the package namespace
 
 from . import loss_functions as _lf
-from . import train_many as _tm
-from .schemas import DatasetSchema, TaskType
-from .models import build_model
-from .noise_types import NoiseConfig
+from . import train_many    as _tm
+from . import schemas       as _sch
+from . import models        as _mdl
+
+# 선택: 없을 수도 있으니 안전하게
+try:
+    from . import noise_types as _nt
+except Exception:
+    _nt = None
 
 # === Loss functions ===
-make_loss   = _lf.make_loss
-ce_loss     = _lf.ce_loss
-gce_loss    = _lf.gce_loss
-focal_loss  = _lf.focal_loss
-cce_loss    = _lf.cce_loss
-scce_loss   = _lf.scce_loss
+make_loss  = _lf.make_loss
+ce_loss    = _lf.ce_loss
+gce_loss   = _lf.gce_loss
+focal_loss = _lf.focal_loss
+cce_loss   = _lf.cce_loss
+scce_loss  = _lf.scce_loss
 
 # === Training ===
 run_experiment     = _tm.run_experiment
-run_clean_vs_noise = _tm.run_clean_vs_noise
+run_clean_vs_noise = getattr(_tm, "run_clean_vs_noise", None)
 plot_history       = _tm.plot_history
-suggest_hparams    = _tm.suggest_hparams
+suggest_hparams    = getattr(_tm, "suggest_hparams", None)
+
+# === Schemas (클래스 바인딩) ===
+DatasetSchema = _sch.DatasetSchema
+TaskType      = _sch.TaskType
+
+# === Models / Noise ===
+build_model = _mdl.build_model
+NoiseConfig = (_nt.NoiseConfig if _nt else None)
 
 __all__ = [
-    "make_loss", "ce_loss", "gce_loss", "focal_loss", "cce_loss", "scce_loss",
-    "run_experiment", "run_clean_vs_noise", "plot_history", "suggest_hparams",
-    "DatasetSchema", "TaskType", "build_model", "NoiseConfig",
+    "make_loss","ce_loss","gce_loss","focal_loss","cce_loss","scce_loss",
+    "run_experiment","run_clean_vs_noise","plot_history","suggest_hparams",
+    "DatasetSchema","TaskType","build_model","NoiseConfig",
 ]
