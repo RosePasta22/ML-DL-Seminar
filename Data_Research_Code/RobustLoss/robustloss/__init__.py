71b621c39abd596086e6e63c07e381123972a8d9
RosePasta22 <danieljoo04@gmail.com>
2025-09-20 07:38:21 +0900

    Export API in __init__; update pyproject

--- Data_Research_Code/RobustLoss/robustloss/__init__.py
+++ Data_Research_Code/RobustLoss/robustloss/__init__.py
@@ -1,41 +1,28 @@
 # robustloss/__init__.py
+# Bind names explicitly into the package namespace (fixes import issues)
 
-# === Loss functions ===
-from .loss_functions import (
-    make_loss,
-    ce_loss,
-    gce_loss,
-    focal_loss,
-    cce_loss,
-    scce_loss,
-)
-
-# === Training ===
-from .train_many import (
-    run_experiment,
-    run_clean_vs_noise,
-    plot_history,
-    suggest_hparams,
-)
-
-# === Schemas ===
+from . import loss_functions as _lf
+from . import train_many as _tm
 from .schemas import DatasetSchema, TaskType
-
-# === Models ===
 from .models import build_model
-
-# === Noise config ===
 from .noise_types import NoiseConfig
 
+# === Loss functions ===
+make_loss   = _lf.make_loss
+ce_loss     = _lf.ce_loss
+gce_loss    = _lf.gce_loss
+focal_loss  = _lf.focal_loss
+cce_loss    = _lf.cce_loss
+scce_loss   = _lf.scce_loss
+
+# === Training ===
+run_experiment     = _tm.run_experiment
+run_clean_vs_noise = _tm.run_clean_vs_noise
+plot_history       = _tm.plot_history
+suggest_hparams    = _tm.suggest_hparams
+
 __all__ = [
-    # loss
     "make_loss", "ce_loss", "gce_loss", "focal_loss", "cce_loss", "scce_loss",
-    # training
     "run_experiment", "run_clean_vs_noise", "plot_history", "suggest_hparams",
-    # schema
-    "DatasetSchema", "TaskType",
-    # model
-    "build_model",
-    # noise
-    "NoiseConfig",
+    "DatasetSchema", "TaskType", "build_model", "NoiseConfig",
 ]
